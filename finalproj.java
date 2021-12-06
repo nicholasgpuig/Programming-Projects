44d1c4004a37a0a65cfd39bd6f871085b7f17ae8
Skyawesome73 <80699428+Skyawesome73@users.noreply.github.com>
2021-12-05 21:53:46 -0600

    Update finalproj.java

--- finalproj.java
+++ finalproj.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.Random;
 public class finalproj
 {
    public static void main(String args[])
@@ -9,21 +8,35 @@ public class finalproj
       System.out.println("Enter in your speed: ");
       int speed = speedInput.nextInt();
       if (speed > 60)
-         System.out.println("Hey you are speeding! You get a $200 ticket");
+         System.out.println("Hey you're speeding! You get a $200 ticket!");
       else if (speed == 60)
-         System.out.println("You're going at the speed limit");
+         System.out.println("You're going at the speed limit; be careful not to go over");
+      else if (speed > 50 && speed < 60)
+        System.out.println("You're under the speed limit but you're going on the fast side");
       else
-         System.out.println("You're going under the speed limit :)");
+         System.out.println("You're under the speed limit but going a bit slow");
          
-      Random randgen = new Random();
-      System.out.println("Enter an upper limit: ");
-      int randbase = speedInput.nextInt();
-      if (randbase >= 1000)
-         System.out.println("Number must be less than 1000");
-      else if (randbase < 0)
-         System.out.println("Number must be positive");
-      int randnum = randgen.nextInt(randbase);
-      System.out.println(randnum);
+      
+      System.out.println("Enter a number in: ");
+      double deci = speedInput.nextDouble();
+      if (deci >= 100)
+        System.out.println("Please enter a number lower than 100");
+      else if (deci < 0)
+        System.out.println("Please enter a positive number");
+      else {
+        int integ = (int)deci;
+        for (;integ <= 100; integ++){
+          if (integ < 33)
+            System.out.println(deci + " " + integ + " There is no char for this integer");
+          else{
+          char letter=(char)integ;
+          System.out.println(deci + " " + integ + " " + letter);
+          deci++;
+          }
+        }
+        }
+      
+      
    }      
 }
 //enter in data and count to xyz
